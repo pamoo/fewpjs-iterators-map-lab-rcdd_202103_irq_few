414b5c1227c35bb2f0de8eae7ba8cc38cf087fbf
Payam Abdullah <payam97abubakr@gmail.com>
2021-05-21 08:25:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Contutorialuctor OO pattern?',
   'implementing Blockchain Web API',
-  'What Does The This Keyword Mean?',
-  'What Is The Contutorialuctor OO Pattern?',
-  'Implementing Blockchain Web API',
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
@@ -12,32 +10,14 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
-  'What Is NaN And How Can We Check For It',
-  'What Is The Difference Between StopPropagation And PreventDefault?',
-  'Immutable State And Pure Functions',
-  'What Is The Difference Between == And ===?',
-  'What Is The Difference Between Event Capturing And Bubbling?',
-  'What Is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
-function map(arr, func){
-    let newArr = []
-    arr.forEach(element => {
-        newArr.push(func(element))
-    });
-    return newArr
-}
-
-function reduce(arr, func, start){
-    let r = (!!start) ? start : arr[0]
-    let i = (!!start) ? 0 : 1
-
-    for (; i < arr.length; i++) {
-      r = func(arr[i], r)
+function titleCased() {
+  return tutorials.map(function (tutorial) {
+    tutorial = tutorial.split(' ');
+    for (var i = 0; i < tutorial.length; i++) {
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
     }
-
-    return r;
-  }
+    return tutorial.join(' ');
+  });
+}
