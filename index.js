63c57d41fc60e8752b7893a1f96b22babe0d4042
Payam Abdullah <payam97abubakr@gmail.com>
2021-05-21 08:25:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Contutorialuctor OO pattern?',
-  'implementing Blockchain Web API',
-  'What Does The This Keyword Mean?',
-  'What Is The Contutorialuctor OO Pattern?',
-  'Implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?',
-  'What Is NaN And How Can We Check For It',
-  'What Is The Difference Between StopPropagation And PreventDefault?',
-  'Immutable State And Pure Functions',
-  'What Is The Difference Between == And ===?',
-  'What Is The Difference Between Event Capturing And Bubbling?',
-  'What Is JSONP?'
-];
 
-const titleCased = () => {
-  return tutorials
-}
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Contutorialuctor OO pattern?',
